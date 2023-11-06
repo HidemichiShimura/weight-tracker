ed6db986c14fb5998d0cfe31735cff631dc4e574
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-11-05 21:55:38 -0800

    add global styles with reset css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <div>App</div>
+    </>
+  );
 }
 
 export default App;
