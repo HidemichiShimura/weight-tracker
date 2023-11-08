43ef7eea839b2a83a775c1d7e0ca8853bd8d6e5f
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-11-07 22:52:15 -0800

    set type to data from handleSubmit

--- frontend/src/components/Pages/Login/LoginPortal.tsx
+++ frontend/src/components/Pages/Login/LoginPortal.tsx
@@ -4,14 +4,19 @@ import { useForm } from "react-hook-form";
 
 import { EMAIL_INPUT_ATTRS, PASSWORD_INPUT_ATTRS, required } from "./constant";
 
+type LoginForm = {
+  email: string;
+  password: string;
+};
+
 export default function LoginPortal() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm<LoginForm>();
 
-  const onSubmit = (data: any) => {
+  const onSubmit = (data: LoginForm) => {
     /* Here will be the logic of login authentication */
     console.log(data);
   };
