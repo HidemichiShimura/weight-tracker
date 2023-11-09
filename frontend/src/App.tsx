2b2e7d8ff7961008c6329dddd4d37929f3bb36a9
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-11-08 20:06:36 -0800

    add the login page routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import { GlobalStyle } from "./components/index";
+import { Login } from "./pages/index";
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route index element={<Login />} />
+        </Routes>
+      </Router>
     </>
   );
 }
