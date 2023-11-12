2730386a90b8ab9ea389f62c233ab53e0c43e751
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-11-11 18:57:34 -0800

    create the signup portal component

---
+++ frontend/src/components/Pages/SignUp/SignUpPortal.tsx
@@ -0,0 +1,64 @@
+import { ReactNode } from "react";
+
+import { useForm } from "react-hook-form";
+
+import {
+  EMAIL_INPUT_ATTRS,
+  PASSWORD_INPUT_ATTRS,
+  INPUTS,
+} from "../Login/constant";
+import {
+  validationSchema,
+  validationSchemaType,
+} from "../../../utils/validationSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  PortalTitle,
+  InputContainer,
+  Label,
+  Button,
+  Input,
+  ErrText,
+} from "../../Style/Portal.styled";
+
+export default function SignUpPortal() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<validationSchemaType>({
+    mode: "onChange",
+    resolver: zodResolver(validationSchema),
+  });
+
+  const onSubmit = (data: validationSchemaType) => {
+    /* Here will be the logic of login authentication */
+    console.log(data);
+  };
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <PortalTitle>Signup</PortalTitle>
+      {INPUTS.map((input, idx) => {
+        const attrs =
+          input === "email" ? EMAIL_INPUT_ATTRS : PASSWORD_INPUT_ATTRS;
+        const errMsg =
+          input === "email"
+            ? (errors.email?.message as ReactNode)
+            : (errors.password?.message as ReactNode);
+
+        return (
+          <InputContainer key={idx}>
+            <Label>
+              {input.replace(input.charAt(0), input.charAt(0).toUpperCase())}
+            </Label>
+            <Input {...register(input)} {...attrs} />
+            <ErrText>{errMsg}</ErrText>
+          </InputContainer>
+        );
+      })}
+      <Button type="submit">Signup</Button>
+    </Form>
+  );
+}
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -3,3 +3,5 @@ export { GlobalStyle } from "./Style/GlobalStyle.styled";
 export { default as LoginSignupLayout } from "./Layout/LoginSignupLayout";
 
 export { default as LoginPortal } from "./Pages/Login/LoginPortal";
+
+export { default as SignUpPortal } from "./Pages/SignUp/SignUpPortal";
