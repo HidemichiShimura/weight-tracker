de1db6a47c86d35116a74b0d20e587e199918ea4
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-11-13 20:20:56 -0800

    create DataDisplay component

---
+++ frontend/src/components/Pages/Home/DataDisplay.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+import styled from "styled-components";
+
+import WeightData from "./WeightData";
+import Toggle from "./common/Toggle";
+import HistoryData from "./HistoryData/HistoryData";
+import { BlurContainer } from "../../Style/BlurContainer.styled";
+
+// These are mock weight data and to be replaced with the actual data later
+const LATEST_DATA = { weight: "62.5", date: "2023-11-23" };
+const DATA_LIST = [
+  { weight: "62.5", date: "2023-11-23" },
+  { weight: "62.5", date: "2023-11-23" },
+  { weight: "62.5", date: "2023-11-23" },
+];
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 25px 50px;
+`;
+const ToggleContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export default function DataDisplay() {
+  const [selected, setSelected] = useState("latest");
+
+  return (
+    <BlurContainer>
+      <Container>
+        <ToggleContainer>
+          <Toggle isSelected={selected === "latest"}>Latest</Toggle>
+          <Toggle isSelected={selected === "history"}>History</Toggle>
+        </ToggleContainer>
+        {selected === "latest" ? (
+          <WeightData data={LATEST_DATA} />
+        ) : (
+          <HistoryData dataList={DATA_LIST} />
+        )}
+      </Container>
+    </BlurContainer>
+  );
+}
