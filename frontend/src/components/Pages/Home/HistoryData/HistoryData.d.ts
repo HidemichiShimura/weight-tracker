9d1ef35f9831b1661168219f4007e771bf015734
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-11-13 17:04:40 -0800

    create HistoryData component

---
+++ frontend/src/components/Pages/Home/HistoryData/HistoryData.d.ts
@@ -0,0 +1,2 @@
+type Data = { weight: string; date: string };
+export type HistoryDataProps = { dataList: Array<Data> };
---
+++ frontend/src/components/Pages/Home/HistoryData/HistoryData.styled.ts
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+import { DEVICE_WIDTH, FONT_SIZE } from "../../../../utils/constants";
+
+export const Table = styled.table`
+  border: 1px solid rgba(245, 245, 245, 0.2);
+  border-collapse: collapse;
+`;
+export const HeaderTr = styled.tr`
+  font-size: ${FONT_SIZE.md};
+  color: var(--primary-white-color);
+  background-color: rgba(245, 245, 245, 0.2);
+  border-collapse: collapse;
+
+  @media (min-width: ${DEVICE_WIDTH.tablet}) {
+    font-size: ${FONT_SIZE.lg};
+  }
+  @media (min-width: ${DEVICE_WIDTH.laptop}) {
+    font-size: ${FONT_SIZE.xl};
+  }
+`;
+export const Th = styled.th`
+  padding: 10px 20px;
+`;
+export const Tr = styled.tr`
+  font-size: ${FONT_SIZE.sm};
+
+  @media (min-width: ${DEVICE_WIDTH.tablet}) {
+    font-size: ${FONT_SIZE.md};
+  }
+  @media (min-width: ${DEVICE_WIDTH.laptop}) {
+    font-size: ${FONT_SIZE.lg};
+  }
+`;
+export const Td = styled.td`
+  border: 1px solid rgba(245, 245, 245, 0.2);
+  text-align: center;
+  padding: 10px 20px;
+`;
