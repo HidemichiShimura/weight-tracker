82b3e10e76126edc07dc721da64598a7fdfa3da3
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-11-15 19:08:27 -0800

    implement validation schema with react-hook-form

--- frontend/src/components/Pages/Home/Menu/ModForm/ModForm.tsx
+++ frontend/src/components/Pages/Home/Menu/ModForm/ModForm.tsx
@@ -1,3 +1,5 @@
+import { useForm } from "react-hook-form";
+
 import {
   ErrText,
   Form,
@@ -7,19 +9,38 @@ import {
   Button,
 } from "../common/Form.styled";
 import { WEIGHT_INPUT_ATTRS, DATE_INPUT_ATTRS } from "../common/attribute";
+import {
+  ModValidationSchema,
+  ModValidationSchemaType,
+} from "@utils/CUDValidationSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export default function ModForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ModValidationSchemaType>({
+    mode: "onChange",
+    resolver: zodResolver(ModValidationSchema),
+  });
+
+  const onSubmit = (data: ModValidationSchemaType) => {
+    /* Here will be the logic of the update operation */
+    console.log(data);
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit(onSubmit)}>
       <InputContainer>
         <Label>Weight</Label>
-        <Input {...WEIGHT_INPUT_ATTRS} />
-        <ErrText></ErrText>
+        <Input {...WEIGHT_INPUT_ATTRS} {...register("weight")} />
+        <ErrText>{errors.weight?.message}</ErrText>
       </InputContainer>
       <InputContainer>
         <Label>Date</Label>
-        <Input {...DATE_INPUT_ATTRS} />
-        <ErrText></ErrText>
+        <Input {...DATE_INPUT_ATTRS} {...register("date")} />
+        <ErrText>{errors.date?.message}</ErrText>
       </InputContainer>
       <Button type="submit">Modify</Button>
     </Form>
