58f8514f0d99438a11d70f4d1b4c9e3cb316b73f
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-11-15 19:46:59 -0800

    fix export paths of components related to Home

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -8,11 +8,11 @@ export { default as LoginPortal } from "./Pages/Login/LoginPortal";
 
 export { default as SignUpPortal } from "./Pages/SignUp/SignUpPortal";
 
-export { default as Header } from "./Pages/Home/Header";
-export { default as WeightData } from "./Pages/Home/WeightData";
-export { default as HistoryData } from "./Pages/Home/HistoryData/HistoryData";
+export { default as Header } from "./Pages/Home/Header/Header";
+export { default as WeightData } from "./Pages/Home/DataDisplay/WeightData/WeightData";
+export { default as HistoryData } from "./Pages/Home/DataDisplay/HistoryData/HistoryData";
 export { default as Toggle } from "./Pages/Home/common/Toggle";
-export { default as DataDisplay } from "./Pages/Home/DataDisplay";
+export { default as DataDisplay } from "./Pages/Home/DataDisplay/DataDisplay";
 export { default as AddForm } from "./Pages/Home/Menu/AddForm/AddForm";
 export { default as ModForm } from "./Pages/Home/Menu/ModForm/ModForm";
 export { default as DelForm } from "./Pages/Home/Menu/DelForm/DelForm";
