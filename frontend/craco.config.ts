4180cc3358162cd0f9c8351c238d540ff74110dd
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-11-17 13:23:46 -0800

    fix webpack error

--- frontend/craco.config.ts
+++ frontend/craco.config.ts
@@ -1,7 +1,37 @@
 import path from "path";
+import webpack, { Configuration } from "webpack";
+
+interface CracoConfig {
+  webpack?: {
+    configure?: (webpackConfig: Configuration) => Configuration;
+  };
+}
 
 export default {
   webpack: {
+    configure: (webpackConfig: Configuration) => {
+      webpackConfig.plugins = webpackConfig.plugins || [];
+
+      webpackConfig.plugins.push(
+        new webpack.ProvidePlugin({
+          process: "process/browser",
+        })
+      );
+
+      webpackConfig.resolve = {
+        ...(webpackConfig.resolve || {}),
+        fallback: {
+          ...(webpackConfig.resolve?.fallback || {}),
+          path: require.resolve("path-browserify"),
+          os: require.resolve("os-browserify/browser"),
+          crypto: require.resolve("crypto-browserify"),
+          stream: require.resolve("stream-browserify"),
+          process: require.resolve("process/browser"),
+        },
+      };
+
+      return webpackConfig;
+    },
     alias: {
       "@components": path.resolve(__dirname, "./src/components/index.ts"),
       "@pages": path.resolve(__dirname, "./src/pages/index.ts"),
@@ -11,5 +41,6 @@ export default {
         "./src/utils/functions/index.ts"
       ),
     },
+    type: ["node"],
   },
 };
