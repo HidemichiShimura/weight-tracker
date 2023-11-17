63e6882e701bbd033ca243b2c8b5bb4ebebf7b9e
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-11-16 22:55:58 -0800

    activate router

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,11 @@
 import express from "express";
 import "dotenv/config";
 
+import { router } from "./router/routes";
+
 const app = express();
+
+app.use(router);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running at PORT:${process.env.PORT}`);
