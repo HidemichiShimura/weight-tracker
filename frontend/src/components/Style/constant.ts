636225fb34785f5aa42ba22f5889829842a427cb
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-11-20 11:25:48 -0800

    replace CSS variables with JS consts

--- frontend/src/components/Style/constant.ts
+++ frontend/src/components/Style/constant.ts
@@ -8,3 +8,16 @@ export const FONT_SIZE = {
   lg: "20px",
   xl: "22px",
 };
+export const COLOR = {
+  blue: "#274c77",
+  white: "#f5f5f5",
+  red: "#d64929",
+  bgBlueMesh:
+    "radial-gradient(at 80% 0%, hsla(189, 100%, 56%, 1) 0px, transparent 50%), radial-gradient(at 80% 50%, hsla(333, 0%, 97%, 1) 0px, transparent 50%), radial-gradient(at 0% 100%, hsla(207, 74%, 79%, 1) 0px, transparent 50%)",
+};
+export const PADDING = {
+  main: "16px",
+};
+export const BORDER_RADIUS = {
+  main: "8px",
+};
