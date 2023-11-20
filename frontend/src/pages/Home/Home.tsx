ba28ac56993e72dfa86c5d8267bddcd96f7490bc
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-11-20 12:10:30 -0800

    replace css vars with js consts

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,11 +1,14 @@
 import styled from "styled-components";
+
 import { HomeLayout, Header, DataDisplay, Menu } from "../../components/index";
+
+import { COLOR } from "../../components/Style/constant";
 
 const BlueMeshBg = styled.div`
   min-height: 100vh;
   min-width: 100vw;
-  background-color: var(--primary-blue-color);
-  background-image: var(--blue-mesh-bg-img);
+  background-color: ${COLOR.blue};
+  background-image: ${COLOR.bgBlueMesh};
 `;
 
 export default function Home() {
